2b395995614c9f28c6752fccff4ad029940de756
aali <git@suvi.cigital.com>
2015-01-13 23:03:07 -0500

    Working access controls and allows fetching credentials.

--- mortimer/mortimer/models.py
+++ mortimer/mortimer/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class AppID(models.Model):
+    app_user = models.ForeignKey(User)
     app_name = models.CharField(max_length=100)
     app_desc = models.CharField(max_length=500)
 
+    class Meta:
+        unique_together = (("app_user","app_name"),)
+
 class AppData(models.Model):
     app_id = models.ForeignKey(AppID)
-    username = models.CharField(max_length=20)
-    enc_password = models.CharField(max_length=32)
+    username = models.CharField(max_length=25)
+    enc_password = models.CharField(max_length=64)
+
+    class Meta:
+        unique_together = (("app_id","username"),)
 
--- mortimer/mortimer/views.py
+++ mortimer/mortimer/views.py
@@ -1,24 +1,67 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
 from django.utils.decorators import method_decorator
-from mortimer.models import AppID
+from mortimer.models import AppID, AppData
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.template import RequestContext, loader
 
 @login_required(redirect_field_name=None,login_url='/login/')
 def home(request):
+    """
     output = ""
-    for app in AppID.objects.all():
+    for app in AppID.objects.filter(app_user=request.user):
         output += "<div> name: %s, desc: %s </div><br>" %(app.app_name,app.app_desc)
+    output += """
+
+    # return HttpResponse(output)
+
+    login_template = loader.get_template('index.html')
+    context = RequestContext(request,{'apps':AppID.objects.filter(app_user=request.user)})
+    return HttpResponse(login_template.render(context))
+
+@login_required(redirect_field_name=None,login_url='/login/')
+def get_creds(request):
+
+    # if GET request, send blank page
+    if not request.POST:
+        return HttpResponse()
+
+    output = ""
+    if AppID.objects.get(id=request.POST['id']).app_user == request.user:
+        for c in AppData.objects.filter(app_id=request.POST['id']):
+            output += "<div> user: %s, pass: %s </div><br>" %(c.username,c.enc_password)
 
     return HttpResponse(output)
+
+"""
+POST requests
+create_app - creates a new AppID for the current user (needs app_name & app_desc)
+remove_app - deletes an AppID that the current must have access to (needs app_name only)
+modify_app - allows user to modify app_name, app_desc or any of the associated creds
+
+send_creds - allows user to send some or all their creds to another user (clone in db)
+"""
+def create_app(request):
+    pass
+
+def remove_app(request):
+    pass
+
+def modify_app(request):
+    pass
+
+def send_creds(request):
+    pass
+
+# ---------------------
+# Authentication Views
 
 def log_in(request):
     login_template = loader.get_template('login.html')
     context = RequestContext(request, {})
     return HttpResponse(login_template.render(context))
-
 
 def authN(request):
     # Stolen from django site
