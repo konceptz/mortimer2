d1e651f67a7f4d501ecda7f0f689c7f6d5332b86
Aman Ali <aali07@students.poly.edu>
2015-01-15 15:34:54 -0500

    Added jQuery call - fixed.

--- mortimer/mortimer/urls.py
+++ mortimer/mortimer/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'mortimer.views.home', name='home'),
-    url(r'^home/', 'mortimer.views.home'),                 # page that skips auth, for now
+    url(r'^home/', 'mortimer.views.home'),
+    url(r'^get_creds/', 'mortimer.views.get_creds'),
     url(r'^login/', 'mortimer.views.log_in', name='log_in'),
     url(r'^logout/', 'mortimer.views.logout_view', name='logout_view'),
     url(r'^authN/', 'mortimer.views.authN', name='authN'),
